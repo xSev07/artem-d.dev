c07048a2e0bf5cc5f3058d51fa74a0119afdf610
SEV <artemiy.dmitrochenko@gmail.com>
2026-07-20 19:14:34 +0400

    added new privacy page

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,22 @@
+import i18next from "i18next";
+import en from "./src/locales/en.json" with { type: "json" };
+import ru from "./src/locales/ru.json" with { type: "json" };
+
+await i18next.init({
+  lng: "en",
+  resources: {
+    en: { translation: en },
+    ru: { translation: ru },
+  },
+});
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addFilter("t", (key, lang, options = {}) =>
+    i18next.t(key, { lng: lang, returnObjects: true, ...options }),
+  );
 
   return {
     dir: {
